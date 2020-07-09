8398e8540f8c719d5ba6b80bf443b8bbc4d0f511
zorigt <eastmod@163.com>
2020-07-09 21:46:27 +0800

    [ init project ]

---
+++ src/main/java/com/zvvnmod/meco/MecoApplication.java
@@ -0,0 +1,17 @@
+package com.zvvnmod.meco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * AUTHOR: zorigt
+ * DATE  : 2020/7/9
+ * WEEK  : Thu
+ * TIME  : 20:50
+ */
+@SpringBootApplication
+public class MecoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MecoApplication.class, args);
+    }
+}
